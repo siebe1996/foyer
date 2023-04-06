18af7316e17e45d5c53c4898e4289b461c77bedd
siebe.vandevoorde <siebe.vandevoorde@student.odisee.be>
2023-04-06 20:09:19 +0200

    added resources

---
+++ app/app/Http/Resources/CompetitionResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CompetitionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'winner_id' => $this->relationLoaded('winner') ? new TeamResource($this->winner) : $this->winner_id,
+        ];
+    }
+}
